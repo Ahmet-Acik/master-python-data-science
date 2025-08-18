ef29df19a8e496542288f340be6137eab27d23ce
drahmetacik <a.acik@icloud.com>
2025-08-18 14:29:17 +0100

    Add seaborn examples for data visualization and dataset loading

--- data_science/seaborn_intro.py
+++ data_science/seaborn_intro.py
@@ -3,4 +3,23 @@ seaborn_intro.py
 ----------------
 Introduction to seaborn for statistical visualization.
 """
-# Add seaborn examples here
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Example seaborn plot: tips dataset
+tips = sns.load_dataset("tips")
+sns.scatterplot(data=tips, x="total_bill", y="tip", hue="day")
+plt.title("Total Bill vs Tip by Day")
+plt.show()
+
+print(sns.get_dataset_names())
+iris = sns.load_dataset("iris")
+titanic = sns.load_dataset("titanic")
+penguins = sns.load_dataset("penguins")
+flights = sns.load_dataset("flights")
+diamonds = sns.load_dataset("diamonds")
+planets = sns.load_dataset("planets")
+exercise = sns.load_dataset("exercise")
+fmri = sns.load_dataset("fmri")
+tips = sns.load_dataset("tips")
+car_crashes = sns.load_dataset("car_crashes")
